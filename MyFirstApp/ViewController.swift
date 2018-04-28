639a916da5275e9548888fc07a5b8fc1e043f47a
Fahri MacBook <fahri-can@web.de>
2018-04-28 18:30:32 +0200

    first iOS app

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -8,11 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var firstField: UITextField!
+    
+    @IBOutlet weak var secondField: UITextField!
+    
+    @IBOutlet weak var toggle: UISegmentedControl!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var firstNum: UILabel!
+    
+    @IBOutlet weak var secondNum: UILabel!
+    
+    @IBOutlet weak var solution: UILabel!
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+     
+        let firstVal: Int = Int(firstField.text!)!
+        let secondVal: Int = Int(secondField.text!)!
+        var result = 0
+        
+        firstNum.text = String(firstVal)
+        secondNum.text = String(secondVal)
+        
+        if toggle.selectedSegmentIndex == 0{
+                result = firstVal + secondVal
+                solution.text = String(result)
+        }
+        else {
+            let result = firstVal - secondVal
+            solution.text = String(result)
+        }
+        
+        self.progressBar.progress = Float(result) / 100.0
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        firstField.delegate = self
+        secondField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +58,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
 
 }
 
